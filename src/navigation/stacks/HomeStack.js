1c12195bb23c2b56502b4ba923b8c5b1f5a19734
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-03-15 13:50:13 +0100

    Navigates to an existing screen in the tab navigator

--- src/navigation/stacks/HomeStack.js
+++ src/navigation/stacks/HomeStack.js
@@ -3,15 +3,21 @@ import { StyleSheet } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/HomeScreen';
 import HomeAlbumScreen from '../../screens/HomeAlbumScreen';
+import ProfileAlbumScreen from '../../screens/ProfileAlbumScreen';
+import SearchAlbumScreen from '../../screens/SearchAlbumScreen';
+import SettingAlbumScreen from '../../screens/SettingAlbumScreen';
 
 const Stack = createNativeStackNavigator();
 
 const HomeStack = () => {
-    
+
     return (
         <Stack.Navigator initialRouteName='Home'>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="HomeAlbum" component={HomeAlbumScreen} />
+            <Stack.Screen name="ProfileAlbum" component={ProfileAlbumScreen} />
+            <Stack.Screen name="SearchAlbum" component={SearchAlbumScreen} />
+            <Stack.Screen name="SettingAlbum" component={SettingAlbumScreen} />
         </Stack.Navigator>
     );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const HomeScreen = ({ route, navigation }) => {
-
+console.log(navigation)
     return (
         <View style={{ flex: 1 }}>
             <Text>Home Screen</Text>
@@ -10,6 +10,21 @@ const HomeScreen = ({ route, navigation }) => {
                 <Text>Navigate to HomeAlbum</Text>
             </TouchableOpacity>
             <Text>{`Cosas del album: ${route.params?.cosas ?? "Aún nada"}`}</Text>
+            <TouchableOpacity 
+                onPress={() => navigation.navigate('Profile', { screen: 'ProfileAlbum' })}
+            >
+                <Text>Navigate to Profile Album</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+                onPress={() => navigation.navigate('Search', { screen: 'SearchAlbum' })}
+            >
+                <Text>Navigate to Search Album</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+                onPress={() => navigation.navigate('Setting', { screen: 'SettingAlbum' })}
+            >
+                <Text>Navigate to Setting Album</Text>
+            </TouchableOpacity>
         </View>
     )
 }
